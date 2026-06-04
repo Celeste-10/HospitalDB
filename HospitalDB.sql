9efc7412961df134e2872d31ceb8e2da1ae7634e
Maria Celeste Carrasco Obando <mccarrasco@uamv.edu.ni>
2026-06-04 08:17:48 -0600

    feat: Se creo la tabla Medicos

--- HospitalDB.sql
+++ HospitalDB.sql
@@ -26,3 +26,23 @@ create table Personal.Especialidades(
 	constraint PK_Especialidades primary key (Id_Especialidad)
 )
 go
+
+--Tabla Medicos
+create table Personal.Medicos(
+	Id_Medico int identity (1,1),
+	Nombre_Medico varchar(50) not null,
+	Correo_Medico varchar(100) not null,
+	Salario decimal(10,2) not null,
+	Id_Especialidad int null,
+
+	Created_at datetime not null default getdate(),
+	Updated_at datetime null,
+	Deleted_at datetime null,
+
+	constraint PK_Medicos primary key (Id_Medico),
+	constraint UQ_CorreoMedico unique (Correo_Medico),
+	constraint CK_SalarioMedico check (Salario > 0),
+	constraint FK_Medicos_Especialidades foreign key (Id_Especialidad) 
+		references Personal.Especialidades(Id_Especialidad)
+)
+go
