31fc3abe1b9890359a282a01852032267d9d4a5d
Maria Celeste Carrasco Obando <mccarrasco@uamv.edu.ni>
2026-06-04 07:58:31 -0600

    feat: Se agregaron los schemas

--- HospitalDB.sql
+++ HospitalDB.sql
@@ -1,3 +1,14 @@
 --Creacion de la base de datos
 create database HospitalDB
 use HospitalDB
+
+--Mostrar todas las bases de datos existentes
+show databases
+
+--Creacion de esquemas
+create schema Personal
+create schema Pacientes
+create schema Atencion
+create schema Farmacia
+
+--Creacion de las tablas con sus restricciones
