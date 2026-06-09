522238e68fc24e4e068623b3b58f09ef3108293b
Maria Celeste Carrasco Obando <mccarrasco@uamv.edu.ni>
2026-06-09 08:24:42 -0600

    feat: Se realizaron modificaciones en la estructuras

--- EmpresaSQL.sql
+++ EmpresaSQL.sql
@@ -61,3 +61,60 @@ create table TEmpleadoProyecto(
 	constraint FK_EmpProj_Empleado foreign key (nEmpleadoID) references TEmpleado(nEmpleadoID),
 	constraint FK_Emproj_Proyecto foreign key (nProyectoID) references TProyecto(nProyectoID)
 )
+
+-- ================================================
+--   Parte 2: Modificacion de estructuras (ALTER)
+-- ================================================
+
+--Agregar columna cEmail a TEmpleado
+alter table TEmpleado add cEmail varchar(100)
+
+--Agregar columna cTelefono
+alter table TEmpleado add cTelefono varchar(15)
+
+--Modificar longitud de cNombre a 100 caracteres
+alter table TEmpleado alter column cNombre varchar(100) not null
+
+--Modificar longitud de cApellido a 100 caracteres
+alter table TEmpleado alter column cApellido varchar (100) not null
+
+--Agregar columna cDireccion
+alter table TEmpleado add cDireccion varchar(250)
+
+--Agregar columna nEdad
+alter table TEmpleado add nEdad int
+
+--Crear restricción CHECK para edades entre 18 y 65 años
+alter table TEmpleado
+add constraint CHK_Empleado_Edad check (nEdad between 18 and 65)
+
+--Agregar restricción UNIQUE al correo electrónico
+alter table TEmpleado
+add constraint UQ_Empleado_Email unique (cEmail)
+
+--Agregar columna bActivo tipo BIT con valor por defecto 1
+alter table TEmpleado add bActivo bit constraint DF_Empleado_Activo default 1
+
+--Eliminar la columna cDireccion
+alter table TEmpleado drop column cDireccion
+
+--Cambiar el tipo de dato de teléfono a VARCHAR(20)
+alter table TEmpleado alter column cTelefono varchar(20)
+
+--Agregar columna cGenero
+alter table TEmpleado add cGenero char(1)
+
+--Agregar restricción CHECK para que el género solo permita M o F
+alter table TEmpleado
+add constraint CHK_Empleado_Genero check (cGenero in('M', 'F'))
+
+--Agregar columna dFechaNacimiento
+alter table TEmpleado add dFechaNacimiento date
+
+--Crear una nueva tabla llamada TSucursal
+create table TSucursal(
+	nSucursalID int identity (1,1) primary key,
+	cNombreSucursal varchar(100) not null,
+	cUbicacion varchar(150) 
+)
+
