978962db7b11a27949709e15d16489fa48a6ab5d
Maria Celeste Carrasco Obando <mccarrasco@uamv.edu.ni>
2026-06-09 07:21:55 -0600

    feat: Se creo la base de datos

---
+++ EmpresaSQL.sql
@@ -0,0 +1,9 @@
+
+-- =============================================
+--  Parte 1: Creacion de base de datos y tablas
+-- =============================================
+create database EmpresaSQL
+go
+
+use EmpresaSQL
+go
